ca3597ce1d311e4601c79eee9321e6fc046c2256
Luca Properzi <abramoproperzi@gmail.com>
2020-11-05 17:21:00 -0700

    Add files via upload

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,18 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breed = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  const data = JSON.parse(body);  
-  if (data.length === 0) {
-    console.log(`Error: These are not the cats you're looking for..`);
-  }else {
-    const description = data[0]['description'];
-    console.log(description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`Error: These are not the cats you're looking for..`);
+    } else {
+      const description = data[0].description;
+      callback(null, description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
